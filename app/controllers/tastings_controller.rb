98c28df9d27c28145d3e968336b13da434be7998
Alexandre Fraschini <alexandre.fraschini@gmail.com>
2020-07-22 18:05:51 +0200

    Filled tastings controller + created some views (new, edit, show)

--- app/controllers/tastings_controller.rb
+++ app/controllers/tastings_controller.rb
@@ -1,2 +1,45 @@
 class TastingsController < ApplicationController
+  before_action :set_tasting, only: [:show, :edit, :update, :destroy]
+  def index
+    @tastings = Tasting.all
+  end
+
+  def show
+  end
+
+  def new
+    @tasting = Tasting.new
+  end
+
+  def create
+    @tasting = Tasting.new(tasting_params)
+    @tasting.save
+
+    redirect_to user_tasting_path(@tasting)
+  end
+
+  def edit
+  end
+
+  def update
+    @tasting.update(tasting_params)
+
+    redirect_to user_tasting_path(@tasting)
+  end
+
+  def destroy
+    @tasting.destroy
+
+    redirect_to user_tastings_path
+  end
+
+  private
+
+  def set_tasting
+    @tasting = Tasting.find(params[:id])
+  end
+
+  def tasting_params
+    params.require(:tasting).permit(:category, :name, :producer, :type, :alcohol, :price, :rating, :comment)
+  end
 end
