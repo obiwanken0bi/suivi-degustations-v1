e286af67fa0eb00e50661edb6750f438e82baa2c
Alexandre Fraschini <alexandre.fraschini@gmail.com>
2020-08-06 10:14:54 +0200

    Changed validations for beer and wine tastings models

--- app/models/wine_tasting.rb
+++ app/models/wine_tasting.rb
@@ -4,13 +4,13 @@ class WineTasting < ApplicationRecord
   has_one_attached :photo
 
   validates :name, presence: true,
-                   length: { maximum: 30 }
+                   length: { maximum: 35 }
   validates :producer, presence: true,
-                       length: { maximum: 30 }
+                       length: { maximum: 35 }
   validates :color, presence: true
   validates :vintage, presence: true
   validates :rating, presence: true,
                      numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :comment, presence: true,
-                      length: { in: 2..100 }
+                      length: { in: 2..300 }
 end
